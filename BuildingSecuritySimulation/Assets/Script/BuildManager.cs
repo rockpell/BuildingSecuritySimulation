2686a1c087c9b14ae91e8d1874f9d8f32216277d
DESKTOP-GKJHOIN\mit-000 <aythffk@gmail.com>
2018-10-31 00:02:21 +0900

    빌드매니저에 타일 생성 추가

---
+++ BuildingSecuritySimulation/Assets/Script/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CreateTile(10, 10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateTile(int width, int height)
+    {
+        GameObject _nomarlTile = Resources.Load<GameObject>("Prefabs/nomarl_tile");
+
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Instantiate(_nomarlTile, new Vector3(2*i, 2*j, 0), Quaternion.identity);
+            }
+        }
+        
+    }
+}
