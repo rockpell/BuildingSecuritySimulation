91623f8ac463d216f94a00bff5b32482d20a5287
younggeun <pyg1007>
2018-11-13 19:37:26 +0900

    캐릭터 방향키로 움직이기(샘플), 이미지추가, UI이미지적용

--- BuildingSecuritySimulation/Assets/Script/Character.cs
+++ BuildingSecuritySimulation/Assets/Script/Character.cs
@@ -9,7 +9,7 @@ public class Character : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-		
+        GetAuthority();
 	}
 	
 	// Update is called once per frame
@@ -17,13 +17,37 @@ public class Character : MonoBehaviour {
 		
 	}
 
+    void FixedUpdate()
+    {
+        if (GetAuthority() == true)
+        {
+            Move();
+            Interaction();
+        }
+    }
+
     public bool GetAuthority()
     {
         return authority;
     }
     public void Move()
     {
-
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down);
+            }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right);
+        }
     }
     public void Interaction()
     {
