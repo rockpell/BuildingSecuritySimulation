e8839105583463f0bd34e549f2cf409980c2a07b
rockpell <aythffk@gmail.com>
2018-11-16 23:21:22 +0900

    저장 기능 추가 중

--- BuildingSecuritySimulation/Assets/Script/FileManager.cs
+++ BuildingSecuritySimulation/Assets/Script/FileManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class FileManager : MonoBehaviour {
     public static FileManager instance;
+    public JsonWrapper jsonWrapper;
+
     string filePath;
+
     private void Awake()
     {
         if (instance == null)
@@ -12,14 +15,43 @@ public class FileManager : MonoBehaviour {
     }
     private void Start()
     {
+        jsonWrapper = new JsonWrapper();
     }
+
     public void Save()
     {
+        List<TileData> _tileDataList = new List<TileData>();
+        string _resultJson;
+        Transform _tiles = GameObject.Find("Tiles").transform;
+
+        for(int i = 0; i < _tiles.childCount; i++)
+        {
+            //_tileDataList.Add(new TileData(_tiles.GetChild(i).position,
+            //    _tiles.GetChild(i).GetComponent<Tile>().GetType(),
+            //    _tiles.GetChild(i).GetComponent<Tile>().GetState(),
+            //    _tiles.GetChild(i).GetComponent<Tile>().GetIsSecurity()
+            //    ));
+            _tileDataList.Add(new TileData(
+                _tiles.GetChild(i).GetComponent<Tile>().GetType(),
+                _tiles.GetChild(i).GetComponent<Tile>().GetState(),
+                _tiles.GetChild(i).GetComponent<Tile>().GetIsSecurity()
+                ));
+        }
+
+        jsonWrapper.tiledatas = _tileDataList.ToArray();
+        Debug.Log(jsonWrapper.tiledatas.Length);
+
+
+        //_resultJson = JsonUtility.ToJson(jsonWrapper, true);
+        _resultJson = JsonUtility.ToJson(new Test(new TileData(type.Blank, false, false)), true);
+        Debug.Log(_resultJson);
     }
     public void Load()
     { 
+
     }
     public void SaveLog()
     {
+
     }
 }
---
+++ BuildingSecuritySimulation/Assets/Script/TileData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class TileData {
+    //public Vector3 position;
+    public int tileType;
+    public bool state;
+    public bool isSecurity;
+
+    //public TileData(Vector3 position, type tileType, bool state, bool isSecurity)
+    //{
+    //    this.position = position;
+    //    this.tileType = tileType;
+    //    this.state = state;
+    //    this.isSecurity = isSecurity;
+    //}
+
+    public TileData(type tileType, bool state, bool isSecurity)
+    {
+        this.tileType = (int)tileType;
+        this.state = state;
+        this.isSecurity = isSecurity;
+    }
+}
+
+[SerializeField]
+public class Test
+{
+    public TileData tileData;
+
+    public Test(TileData value)
+    {
+        this.tileData = value;
+    }
+}
+
+[SerializeField]
+public class JsonWrapper
+{
+    public TileData[] tiledatas;
+}
